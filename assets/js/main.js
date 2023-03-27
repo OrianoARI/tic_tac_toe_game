73417b382cb13979cac4910b7f2b3c7c3f12d380
Oriano <alfisti@rocketmail.com>
2023-03-27 10:27:43 +0200

    add cpu play fonctionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,21 @@ let cellThree = document.querySelectorAll(".cellThree");
 let turn = 0;
 let gameTableIndex = 0;
 let player = 1;
-console.log(player);
 let gameTable = [
     [],
     [],
     []
 ];
 let gameOver = false;
+let cell = document.querySelectorAll('.cell');
+let randomIndex = random(0, document.querySelectorAll('.cell').length - 1)
+console.log(randomIndex);
 
 
 function checkWin() {
     for (let i = 0; i < gameTable.length; i++) {
         if (gameTable[i][0] != "" && gameTable[i][0] == gameTable[i][1] && (gameTable[i][1]) && gameTable[i][1] != "" && (gameTable[i][1] == gameTable[i][2])) {
-            if (player == 2) {
+            if (player == 1) {
                 console.log("Player 1 gagne");
             } else {
                 console.log("Player 2 gagne");
@@ -28,7 +30,7 @@ function checkWin() {
             gameOver = true;
         }
         if (gameTable[0][i] != "" && gameTable[0][i] == gameTable[1][i] && gameTable[2][i] && gameTable[1][i] != "" && (gameTable[1][i] == gameTable[2][i])) {
-            if (player == 2) {
+            if (player == 1) {
                 console.log("Player 1 gagne");
             } else {
                 console.log("Player 2 gagne");
@@ -36,7 +38,7 @@ function checkWin() {
             gameOver = true;
         }
         if (gameTable[0][0] != "" && gameTable[0][0] == gameTable[1][1] && gameTable[1][1] && gameTable[1][1] != "" && (gameTable[1][1] == gameTable[2][2])) {
-            if (player == 2) {
+            if (player == 1) {
                 console.log("Player 1 gagne");
             } else {
                 console.log("Player 2 gagne");
@@ -44,38 +46,80 @@ function checkWin() {
             gameOver = true;
         }
         if (gameTable[0][2] != "" && gameTable[0][2] == gameTable[1][1] && gameTable[1][1] && gameTable[1][1] != "" && (gameTable[1][1] == gameTable[2][0])) {
-            if (player == 2) {
+            if (player == 1) {
                 console.log("Player 1 gagne");
             } else {
                 console.log("Player 2 gagne");
             }
             gameOver = true;
         } else {
-            if (turn >= 9 && gameTable[i][0] != "" &&  gameTable[i][1] != "" && gameTable[0][i] != "" && gameTable[1][i] != "" && gameTable[0][0] != "" && gameTable[1][1] != "" && gameTable[0][2] != "")
+            if (turn >= 9 && gameTable[i][0] != "" && gameTable[i][1] != "" && gameTable[0][i] != "" && gameTable[1][i] != "" && gameTable[0][0] != "" && gameTable[1][1] != "" && gameTable[0][2] != "") {
                 console.log("draw");
+                gameOver = true
+            }
         }
     }
 }
-
-
 
 function play(elem) {
     if (elem.innerHTML != "X" && elem.innerHTML != "O" && gameOver == false) {
         if (player == 1) {
             elem.innerHTML = "X";
-            player = 2;
-        } else {
-            elem.innerHTML = "O";
-            player = 1;
+            console.log(turn);
+            updateGrid();
+            checkWin();
+            if (turn >= 8) {
+                gameOver = true;
+            }
+            if (!gameOver) {
+                console.log(gameOver);
+                player = 2;
+                cpuPlay();
+            }
         }
-        updateGrid()
-        console.log(gameTable);
         turn++
-        if (turn >= 9) {
-            gameOver = true;
-        }
+
     }
+    if (gameOver == true) {
+        console.log(gameOver);
+    }
+}
+
+function cpuPlay() {
+    let cpuCell = random(0, 8)
+    while (document.querySelectorAll('.cell')[cpuCell].innerHTML != "") {
+        console.log(gameOver);
+        console.log("bakbak");
+        cpuCell = random(0, 8)
+    }
+    document.querySelectorAll(".cell")[cpuCell].innerHTML = "O"
+    turn++;
+    updateGrid();
     checkWin();
+    player = 1;
+    console.log(turn);
+    if (turn >= 9) {
+        gameOver = true;
+    }
+    if (gameOver == true) {
+        console.log(gameOver);
+    }
+}
+
+
+function reset() {
+    cellOne.forEach(element => {
+        element.innerHTML = ""
+    });
+    cellTwo.forEach(element => {
+        element.innerHTML = ""
+    });
+    cellThree.forEach(element => {
+        element.innerHTML = ""
+    });
+    turn = 0;
+    gameOver = false;
+    console.log(gameOver);
 }
 
 function updateGrid() {
@@ -88,7 +132,6 @@ function updateGrid() {
     for (let i = 0; i < cellThree.length; i++) {
         gameTable[2][i] = cellThree[i].innerHTML
     }
-
 }
 
 
